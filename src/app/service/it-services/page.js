79d7e41052d0e1e6b55354ad855a617c4334bf75
TasleemSiddiqui <154926573+TasleemSiddiqui@users.noreply.github.com>
2024-10-04 20:02:58 +0500

    data update

--- src/app/service/it-services/page.js
+++ src/app/service/it-services/page.js
@@ -9,6 +9,7 @@ import Link from "next/link";
 import { itServiceData } from "@/components/ui/HomePage/data";
 import Wrapper from "@/components/shared/Wrapper";
 import { Button } from "@/components/ui/Elements/button";
+import { FaMedapps } from "react-icons/fa6";
 const ItServices = () => {
   return (
     <Wrapper>
@@ -34,7 +35,7 @@ const ItServices = () => {
         </div>{" "}
         <section className="justify-center items-center mx-auto">
           {" "}
-          <Tabs defaultValue="mobileApp">
+          <Tabs defaultValue="MobileApps">
             {" "}
             {/* TabsList for tab navigation */}{" "}
             <TabsList
@@ -64,18 +65,19 @@ const ItServices = () => {
                       className="w-full  rounded-[25px] sm:rounded-[35px] flex flex-col justify-evenly items-start bg-white p-3 sm:p-5  lg:p-7 xl:p-8  duration-500    hover:shadow-2xl max-w-[610px] "
                     >
                       {" "}
-                      <div className="mb-2 flex h-[40px] w-[40px] md:h-[50px] lg:h-[70px] md:w-[50px] lg:w-[70px] items-center justify-center rounded-full bg-[#F0EEFF]">
+                      <div className="mb-2 text-primary flex h-[40px] w-[40px] md:h-[50px] lg:h-[70px] md:w-[50px] lg:w-[70px] items-center justify-center rounded-full bg-[#F0EEFF]">
                         {" "}
-                        <Image
+                        {/* <Image
                           alt="icon"
                           loading="lazy"
                           width={94}
                           height={100}
                           className="lg:h-[32px] lg:w-[36px] md:h-[30px] md:w-[30px] w-[20px] h-[20px]"
                           src={service.src}
-                        />{" "}
+                        />{" "} */}
+                        <FaMedapps size={30} />
                       </div>{" "}
-                      <div className="lg:mx-0 ml-2 flex justify-center items-center">
+                      <div className="lg:mx-0 ml-2 flex justify-center tracking-tight leading-5 items-center">
                         {" "}
                         <h2 className="text-[14px] md:text-[16px] lg:text-[18px] xl:text-[24px] font-[600]">
                           {" "}
@@ -102,6 +104,31 @@ const ItServices = () => {
               )}{" "}
             </div>{" "}
           </Tabs>{" "}
+          <div className=" mx-auto p-10 ">
+            <div className="grid md:grid-cols-2 grid-cols-1 bg-slate-100 rounded-2xl ">
+            <div className=" p-10 rounded-md">
+              <h2 className="text-[14px] md:text-[16px] lg:text-[18px] xl:text-[24px] font-[600]">
+              Free consultation for website development.
+              </h2>
+              <div className="mb-2 min-h-[50px] sm:min-h-[100px] md:min-h-[93px] lg:min-h-[140px]">
+                <p className="break-normal text-[10px] sm:text-[12px] text-gray-400 md:text-[14px] lg:text-[18px] md:max-w-[470px] font-medium">
+                Contact us for free to make your business digitalized and get globally connected with hundreds of users. Get a free consultation to make a powerful social impact through attractive and customized websites.</p>{" "}
+              </div>{" "}
+              <Link
+                href="/contact"
+                className="text-[10px] lg:text-[16px] xl:text-[18px]"
+              >
+                {" "}
+                <Button>Connect now</Button>{" "}
+              </Link>{" "}
+            </div>
+
+            <div className="">
+              <Image src={'/assets/serviceProfit.da66f033.svg'} width={600} height={600} className="w-full" />
+            </div>
+            </div>
+        
+          </div>
         </section>{" "}
       </section>{" "}
     </Wrapper>
